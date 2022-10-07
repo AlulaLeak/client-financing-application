f4d1cce744286454e6cb2288f2317c56da90400c
AlulaLeak <alula.leakemariam94@gmail.com>
2022-10-07 17:07:53 -0400

    added example client id

--- lib/auth/auth_wrapper.dart
+++ lib/auth/auth_wrapper.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:workingauth/pages/signin_page.dart';
 
 class Auth extends StatelessWidget {
   const Auth({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Auth");
+    return const SignInPage();
   }
 }
---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SignInScreen(
+      providerConfigs: const [
+        EmailProviderConfiguration(),
+        GoogleProviderConfiguration(
+            clientId:
+                '508361698170-n364a6flon11fddjhvfk7qjsfi6ma3cc.apps.googleusercontent.com'),
+      ],
+      headerBuilder: (context, constraints, shrinkOffset) {
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset('assets/placeholder.jpg'),
+          ),
+        );
+      },
+      subtitleBuilder: (context, action) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: action == AuthAction.signIn
+              ? const Text('Welcome to FlutterFire, please sign in!')
+              : const Text('Welcome to Flutterfire, please sign up!'),
+        );
+      },
+      footerBuilder: (context, action) {
+        return const Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: Text(
+            'By signing in, you agree to our terms and conditions.',
+            style: TextStyle(color: Colors.grey),
+          ),
+        );
+      },
+      sideBuilder: (context, shrinkOffset) {
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset('assets/placeholder.jpg'),
+          ),
+        );
+      },
+    );
+  }
+}
